745893089c3a1334bfe2c25e09c52287f19aa9a5
Robertson Paul <paulr@dollabs.com>
2020-07-07 10:22:14 -0400

    Changed instance variable naming scheme

--- src/pamela/tools/belief_state_planner/dmcgpcore.clj
+++ src/pamela/tools/belief_state_planner/dmcgpcore.clj
@@ -729,9 +729,9 @@
                                      (with-out-str (print (nth condit 1)))
                                      (with-out-str (print (nth condit 2)))
                                      ")"))
-        (let [first-expn (rtm/evaluate  wrtobject (nth condit 1) nil nil nil nil)
+        (let [first-expn (rtm/evaluate  wrtobject "???" (nth condit 1) nil nil nil nil)
               first-expn (if (rtm/is-lvar? first-expn) (rtm/deref-lvar first-expn) first-expn)
-              second-expn (rtm/evaluate wrtobject (nth condit 2) nil nil nil nil)
+              second-expn (rtm/evaluate wrtobject "???" (nth condit 2) nil nil nil nil)
               second-expn (if (rtm/is-lvar? second-expn) (rtm/deref-lvar second-expn) second-expn)]
           (if (> verbosity 2) (println "(= "
                                        (with-out-str (print (nth condit 1))) "=" first-expn
@@ -742,7 +742,7 @@
       (let [plant (nth condit 1)
             names (nth condit 2)
             args (doall (map (fn [arg]
-                               (rtm/evaluate wrtobject arg nil nil nil nil))
+                               (rtm/evaluate wrtobject "???" arg nil nil nil nil))
                              (rest (rest (rest condit)))))]
         (cond (= plant 'dmcp) ;+++ dmcp handled specially
               (internal-condition-call plant (first names) args)
